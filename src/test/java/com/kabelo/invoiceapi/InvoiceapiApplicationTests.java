cefef1628861f8e2ce6adb5ed5dfad715d873389
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2025-11-20 15:27:51 +0200

    Starter files and code

---
+++ src/test/java/com/kabelo/invoiceapi/InvoiceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kabelo.invoiceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
