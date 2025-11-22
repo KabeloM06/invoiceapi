42ee725a9afe843df77f89a9283d1fda5aee23db
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2025-11-22 09:44:05 +0200

    Users table in schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,28 @@
+CREATE SCHEMA IF NOT EXISTS invoiceapp;
+
+SET NAMES 'UTF8MB4';
+SET TIME_ZONE = 'Africa/Johannesburg';
+SET TIME_ZONE = '+02:00';
+
+USE invoiceapp;
+
+DROP TABLE IF EXISTS Users;
+
+CREATE TABLE Users (
+    id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) DEFAULT NULL,
+    address VARCHAR(255) DEFAULT NULL,
+    phone VARCHAR(20) DEFAULT NULL,
+    title VARCHAR(50) DEFAULT NULL,
+    bio VARCHAR(255) DEFAULT NULL,
+    enabled BOOLEAN DEFAULT TRUE,
+    non_locked BOOLEAN DEFAULT TRUE,
+    using_mfa BOOLEAN DEFAULT FALSE,
+    created_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    image_url VARCHAR(255) DEFAULT 'https://cdn-icons-png.flaticon.com/512/149/149071.png',
+    CONSTRAINT UQ_Users_Email UNIQUE (email)
+
+);
